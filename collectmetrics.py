65f87699f348e280dfee45df29625a318e61c097
hagerElsehaimy <helsehai@HELSEHAI-T490.eg.oracle.com>
2021-11-10 16:13:45 +0200

    rename branch

--- collectmetrics.py
+++ collectmetrics.py
@@ -1,5 +1,12 @@
 import oci
 from datetime import datetime, timedelta
+
+
+compartment_ID = "ocid1.compartment.oc1..aaaaaaaa4vtosolddtixrwkdk43zpoyjehqbnbyy5wqfn4njzthbp3q74d6q"
+OCID ="ocid1.instance.oc1.me-jeddah-1.anvgkljrqvob4kyctrn4pl5ikynze23mbr3kfpilzejfhhnahjave2eit5oa"
+utcTimedelta = 2
+metric_namespace = "oci_computeagent"
+
 # # Create a default config using DEFAULT profile in default location
 # # Refer to
 # # https://docs.cloud.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm#SDK_and_CLI_Configuration_File
@@ -9,19 +16,18 @@ config = oci.config.from_file("config")
 # Initialize service client with default config file
 monitoring_client = oci.monitoring.MonitoringClient(config)
 
-OCID ="ocid1.instance.oc1.me-jeddah-1.anvgkljrqvob4kyctrn4pl5ikynze23mbr3kfpilzejfhhnahjave2eit5oa"
 # Send the request to service, some parameters are not required, see API
 # doc for more info
 #
 
-start_time = datetime.strftime(datetime.now() - timedelta(hours=2, minutes=1), "%Y-%m-%dT%H:%M:%S.%fZ")
-end_time = datetime.strftime(datetime.now()-timedelta(hours=2), "%Y-%m-%dT%H:%M:%S.%fZ")
+start_time = datetime.strftime(datetime.now() - timedelta(hours=utcTimedelta, minutes=1), "%Y-%m-%dT%H:%M:%S.%fZ")
+end_time = datetime.strftime(datetime.now()-timedelta(hours=utcTimedelta), "%Y-%m-%dT%H:%M:%S.%fZ")
 
 summarize_metrics_data_response = monitoring_client.summarize_metrics_data(
-    compartment_id="ocid1.compartment.oc1..aaaaaaaa4vtosolddtixrwkdk43zpoyjehqbnbyy5wqfn4njzthbp3q74d6q",
+    compartment_id=compartment_ID,
     summarize_metrics_data_details=oci.monitoring.models.SummarizeMetricsDataDetails(
-        namespace="oci_computeagent",
-        query='CpuUtilization[1m]{resourceID = "ocid1.instance.oc1.me-jeddah-1.anvgkljrqvob4kyctrn4pl5ikynze23mbr3kfpilzejfhhnahjave2eit5oa"}.mean()',
+        namespace=metric_namespace,
+        query='CpuUtilization[1m]{resourceID = %s}.mean()' %(OCID),
         start_time=start_time,
         end_time=end_time
     ))
