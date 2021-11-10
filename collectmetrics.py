eedf36f668e2f16f3aca0739524c6240210652b7
hagerElsehaimy <hager.m.elsehaimy@gmail.com>
2021-11-10 16:36:34 +0200

    convert into function

--- collectmetrics.py
+++ collectmetrics.py
@@ -11,28 +11,33 @@ metric_namespace = "oci_computeagent"
 # # Refer to
 # # https://docs.cloud.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm#SDK_and_CLI_Configuration_File
 # # for more info
-config = oci.config.from_file("config")
 
-# Initialize service client with default config file
-monitoring_client = oci.monitoring.MonitoringClient(config)
 
-# Send the request to service, some parameters are not required, see API
-# doc for more info
-#
+def collect_metrices(compartment_ID, OCID, utcTimedelta, metric_namespace):
 
-start_time = datetime.strftime(datetime.now() - timedelta(hours=utcTimedelta, minutes=1), "%Y-%m-%dT%H:%M:%S.%fZ")
-end_time = datetime.strftime(datetime.now()-timedelta(hours=utcTimedelta), "%Y-%m-%dT%H:%M:%S.%fZ")
+    config = oci.config.from_file("config")
 
-summarize_metrics_data_response = monitoring_client.summarize_metrics_data(
-    compartment_id=compartment_ID,
-    summarize_metrics_data_details=oci.monitoring.models.SummarizeMetricsDataDetails(
-        namespace=metric_namespace,
-        query='CpuUtilization[1m]{resourceID = %s}.mean()' %(OCID),
-        start_time=start_time,
-        end_time=end_time
-    ))
+    # Initialize service client with default config file
+    monitoring_client = oci.monitoring.MonitoringClient(config)
 
-# Get the data from response
-print(summarize_metrics_data_response.data)
+    # Send the request to service, some parameters are not required, see API
+    # doc for more info
+    #
+
+    start_time = datetime.strftime(datetime.now() - timedelta(hours=utcTimedelta, minutes=1), "%Y-%m-%dT%H:%M:%S.%fZ")
+    end_time = datetime.strftime(datetime.now() - timedelta(hours=utcTimedelta), "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    summarize_metrics_data_response = monitoring_client.summarize_metrics_data(
+        compartment_id=compartment_ID,
+        summarize_metrics_data_details=oci.monitoring.models.SummarizeMetricsDataDetails(
+            namespace=metric_namespace,
+            query='CpuUtilization[1m]{resourceID = %s}.mean()' % (OCID),
+            start_time=start_time,
+            end_time=end_time
+        ))
+
+    # Get the data from response
+    return summarize_metrics_data_response.data
+
 # utilization = summarize_metrics_data_response.data[0].__dict__['_aggregated_datapoints'][0].__dict__['_value']
 # print(utilization)
